3700d57e8b9e4bb30557e22256feab28e7264214
namfon <numfhonpakse@gmail.com>
2026-03-17 01:36:08 +0700

    feat: add order status module

---
+++ src/main/java/com/example/test/repository/OrderStatusRepository.java
@@ -0,0 +1,14 @@
+package com.example.test.repository;
+
+import com.example.test.entity.OrderStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OrderStatusRepository extends JpaRepository<OrderStatus, Integer> {
+
+    Optional<OrderStatus> findByName(String name);
+
+}
