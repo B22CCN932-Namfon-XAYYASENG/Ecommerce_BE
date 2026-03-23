1b4f6828188d849dd7739e51b418e4ffa3db9c91
namfon <numfhonpakse@gmail.com>
2026-03-23 20:50:08 +0700

    feat: add email module

---
+++ src/main/java/com/example/test/entity/EmailDetails.java
@@ -0,0 +1,16 @@
+package com.example.test.entity;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class EmailDetails {
+    String recipient;
+    String msgBody;
+    String subject;
+    String attachment;
+}
