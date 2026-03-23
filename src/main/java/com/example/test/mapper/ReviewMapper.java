4b452a901fb20d416ec5cb66c01e3a1d9656def9
namfon <numfhonpakse@gmail.com>
2026-03-23 20:51:00 +0700

    feat: add review module

---
+++ src/main/java/com/example/test/mapper/ReviewMapper.java
@@ -0,0 +1,42 @@
+package com.example.test.mapper;
+
+import com.example.test.dto.request.ReviewRequest;
+import com.example.test.dto.response.ReviewResponse;
+import com.example.test.entity.Review;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ReviewMapper {
+
+    Review toReview(ReviewRequest request);
+
+    @Mapping(target = "user", expression = "java(buildUser(review))")
+    @Mapping(target = "product", expression = "java(buildProduct(review))")
+    ReviewResponse toReviewResponse(Review review);
+
+    default JsonNode buildUser(Review review){
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        ObjectNode objectNode = objectMapper.createObjectNode();
+
+        objectNode.put("id", review.getUser().getId());
+        objectNode.put("username", review.getUser().getUsername());
+
+        return objectNode;
+    }
+
+    default JsonNode buildProduct(Review review) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        ObjectNode objectNode = objectMapper.createObjectNode();
+
+        objectNode.put("id", review.getProduct().getId());
+        objectNode.put("name", review.getProduct().getName());
+
+        return objectNode;
+    }
+}
